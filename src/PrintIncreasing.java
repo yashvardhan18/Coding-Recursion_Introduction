d2158e14438c0653f25dcb00af908000ee589a08
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-05 04:22:30 +0530

    q2

---
+++ src/PrintIncreasing.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class PrintIncreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code hereScanner scn=new Scanner(System.in);
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        printIncreasing(n);
+    }
+
+    public static void printIncreasing(int n){
+        if(n==0){
+            return;
+
+        }
+        printIncreasing(n-1);
+        System.out.println(n);
+
+
+
+    }
+}
