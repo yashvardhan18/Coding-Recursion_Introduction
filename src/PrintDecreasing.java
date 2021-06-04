3ea6fb26fb210f5fce21041751b7e8ee61758e6d
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-05 03:29:10 +0530

    Initial commit

---
+++ src/PrintDecreasing.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class PrintDecreasing {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+    public static void printDecreasing(int n){
+
+    }
+
+}
