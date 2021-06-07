0f2dd502ffabe3475fd2e6d7182dbf30aec0ce48
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-07 17:21:07 +0530

    q4

---
+++ src/factorial.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class factorial {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int result=factorial(n);
+        System.out.println(result);
+    }
+
+    public static int factorial(int n){
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+}
