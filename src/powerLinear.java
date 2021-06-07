e3ce17083b9f2281a270c2d945022b8dbbc80348
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-07 18:03:35 +0530

    q5

---
+++ src/powerLinear.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        int n=scn.nextInt();
+        int result=power(x,n);
+        System.out.println(result);
+
+    }
+
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        int xnm1=power(x,n-1);
+        int xn=x*xnm1;
+        return xn;
+    }
+
+}
